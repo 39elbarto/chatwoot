1532ee6bb731481d590df3db0f2796c1e0a096b8
Artem Kozhenchenkov <39elbarto@gmail.com>
2026-03-07 23:11:56 +0700

    Fix frontend lint and helper specs

--- app/javascript/dashboard/helper/editorHelper.js
+++ app/javascript/dashboard/helper/editorHelper.js
@@ -580,7 +580,9 @@ const createNode = (editorView, nodeType, content) => {
     }
     case 'cannedResponse': {
       const textContent =
-        typeof content === 'object' && content !== null ? content.text : content;
+        typeof content === 'object' && content !== null
+          ? content.text
+          : content;
       const format =
         typeof content === 'object' && content !== null ? content.format : null;
 
